94da0a0c746bc7b6e14d7fe25548c317f51c4641
Sumit Aggarwal <sumit.aggarwal@algoworks.com>
2022-10-02 14:22:55 +0530

    Saving of student data is working fine

--- CoreDataPractice/CoreDataPractice/DatabaseHelper.swift
+++ CoreDataPractice/CoreDataPractice/DatabaseHelper.swift
@@ -1,4 +1,6 @@
 import Foundation
+import CoreData
+import UIKit
 
 class DatabaseHelper {
     
@@ -7,8 +9,19 @@ class DatabaseHelper {
     }
     
     static let sharedObject = DatabaseHelper()
+    let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
     
     func save(object: [String: Any]) {
+        let student = NSEntityDescription.insertNewObject(forEntityName: "Student", into: context!) as! Student
+        student.name = object["name"] as? String
+        student.address = object["address"] as? String
+        student.city = object["city"] as? String
+        student.mobile = object["mobile"] as? String
         
+        do {
+            try context?.save()
+        } catch {
+            debugPrint("Data not saved")
+        }
     }
 }
